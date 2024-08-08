e27605ff270eb119c12907c82671f86a7d3b6ccb
ansarirayyan01 <ansarirayyan874@gmail.com>
2024-08-08 14:15:40 +0530

    changes in the file

--- postgres/src/table.js
+++ postgres/src/table.js
@@ -5,11 +5,11 @@ const { Client } = require ("pg");
 // Async function to insert data into a table
 async function insertData() {
   const client = new Client({
-    host: "ep-divine-cake-a50bgnxl.us-east-2.aws.neon.tech",
+    host: "Host name",
     port: 5432,
-    database: "Start",
-    user: "Start_owner",
-    password: "DblYk0nVC4Be",
+    database: "Your database",
+    user: "Your username",
+    password: "Your password",
   });
 
   try {
